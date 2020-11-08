56da579756500d3fb323aa1918b2762c2d5c6010
CornetS28 <corsam28@gmail.com>
2020-11-07 23:39:26 -0500

    create a global theme

---
+++ src/util/theme.js
@@ -0,0 +1,30 @@
+export default {
+  palette: {
+    primary: {
+      main: '#333333',
+      contrastWhite: '#fff',
+    },
+    secondary: {
+      main: '#566573',
+      dark: '#808B95',
+      lightGrey: '#ABB2B9',
+      lighter: '#D5D8DC',
+      lightest: '#EAECEE',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+  form: {
+    textAlign: 'center',
+  },
+  // image: {
+  //   margin: '20px auto 20px auto',
+  // },
+  // pageTitle: {
+  //   margin: '10px auto 10px auto',
+  // },
+  // textField: {
+  //   margin: '10px auto 10px auto',
+  // },
+};
