7149d249f68d207ac7775eabd8751e18ac08d7d5
CornetS28 <corsam28@gmail.com>
2020-11-08 20:36:11 -0500

    add navbar bars and fix the othed exited routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider, defaultTheme } from '@adobe/react-spectrum';
-import { View } from '@react-spectrum/view';
 import './App.css';
-import Succes from './users/pages/Succes';
-import RecoverPassword from './users/pages/RecoverPassword';
-import PasswordReset from './users/pages/PasswordReset';
-import Home from './Home';
 import Advocate from './Advocate';
 
+// React Spectrum Stuff
+import { Provider, defaultTheme } from '@adobe/react-spectrum';
+import { View } from '@react-spectrum/view';
+
+// MUI Stuff
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import themeObject from './util/theme';
 
 // Components
 import Navbar from './components/layout/Navbar';
-
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Succes from './components/Succes';
+import RecoverPassword from './components/RecoverPassword';
+import PasswordReset from './components/PasswordReset';
 
 // Screens
-import signup from './screens/signup';
-import login from './screens/login';
 import publicPage from './screens/publicPage';
 
 const theme = createMuiTheme(themeObject);
@@ -35,33 +36,25 @@ function App() {
 
         <div className="container">
           <Switch>
-            {/* <Route exact path="/">
-              <Home />
-            </Route> */}
-            {/* <Route exact path="/login">
-              <Login />
-            </Route> */}
             <Route exact path="/" component={publicPage} />
-            <Route exact path="/signup" component={signup} />
-            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
 
-            {/* <Route exact path="/success">
-              <Succes />
-            </Route>
-            <Route exact path="/recover_password">
-              <RecoverPassword />
-            </Route>
-            <Route exact path="/send_password_reset">
-              <PasswordReset />
-            </Route>
-            <Route exact path="/advocate">
-              <Advocate />
-            </Route> */}
+            <Route exact path="/success" component={Succes} />
+            <Succes />
+
+            <Route exact path="/recover-password" component={RecoverPassword} />
+
+            <Route
+              exact
+              path="/send-password-reset"
+              component={PasswordReset}
+            />
+
+            <Route exact path="/advocate" component={Advocate} />
+
             {/* <Route path="/logout">
                 <Redirect to="/" />
-            </Route> */}
-            {/* <Route path="*">
-              <Redirect to="/" />
             </Route> */}
           </Switch>
         </div>
