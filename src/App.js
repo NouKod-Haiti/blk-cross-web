b00a590a91c2e4c949a0cfe583b8882bb4c2c7a9
CornetS28 <corsam28@gmail.com>
2020-11-08 20:35:49 -0500

    add navbar bars and fix the othed exited routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider, defaultTheme } from '@adobe/react-spectrum';
+import { View } from '@react-spectrum/view';
 import './App.css';
 import Succes from './users/pages/Succes';
 import RecoverPassword from './users/pages/RecoverPassword';
@@ -8,15 +9,30 @@ import PasswordReset from './users/pages/PasswordReset';
 import Home from './Home';
 import Advocate from './Advocate';
 
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import themeObject from './util/theme';
+
+// Components
+import Navbar from './components/layout/Navbar';
+
 
 // Screens
 import signup from './screens/signup';
 import login from './screens/login';
+import publicPage from './screens/publicPage';
 
+const theme = createMuiTheme(themeObject);
 function App() {
   return (
     <Provider theme={defaultTheme} height="100%" id="provider">
       <Router>
+        <View>
+          <MuiThemeProvider theme={theme}>
+            <Navbar />
+          </MuiThemeProvider>
+        </View>
+
         <div className="container">
           <Switch>
             {/* <Route exact path="/">
@@ -25,6 +41,7 @@ function App() {
             {/* <Route exact path="/login">
               <Login />
             </Route> */}
+            <Route exact path="/" component={publicPage} />
             <Route exact path="/signup" component={signup} />
             <Route exact path="/login" component={login} />
 
