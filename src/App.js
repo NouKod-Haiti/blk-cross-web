79c8d6ca577cdccfcb623de0a0faf303d4eb0711
CornetS28 <corsam28@gmail.com>
2020-11-10 13:39:01 -0500

    hotfix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Advocate from './Advocate';
+import Advocate from './screens/advocate';
 
 // React Spectrum Stuff
 import { Provider, defaultTheme } from '@adobe/react-spectrum';
 import { View } from '@react-spectrum/view';
 
 // MUI Stuff
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import themeObject from './util/theme';
 
