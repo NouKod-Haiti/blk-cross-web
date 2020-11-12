12ac1b13a4944e90e97472f7ae5239c0b76676f5
CornetS28 <corsam28@gmail.com>
2020-11-12 03:07:08 -0500

    connect added props and add validations for the signup form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useRef } from 'react';
+// import axios from 'axios';
 import SignupForm from '../screens/signup';
-import env from 'react-dotenv';
+
+
+import { useDispatch, useSelector } from 'react-redux';
+import { isEmail } from 'validator';
+
+import { register } from '../actions/auth';
 
 
 
@@ -13,9 +18,70 @@ function Signup() {
     passwordConfirmation: '',
     organization: '',
   });
-  
-// const token =
-//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc0RldiI6dHJ1ZSwiaWF0IjoxNjA1MTIwNDA1fQ.bsEudzWzt1O56f2VaL2QcuJs4h_DldJpWiTTTnyQrsg';
+  const [successful, setSuccessful] = useState(false);
+  const form = useRef();
+  const checkBtn = useRef();
+   const dispatch = useDispatch();
+
+   const required = (value) => {
+     if (!value) {
+       return (
+         <div>
+           This field is required!
+         </div>
+       );
+     }
+   };
+
+   const validEmail = (value) => {
+     if (!isEmail(value)) {
+       return (
+         <div>
+           This is not a valid email.
+         </div>
+       );
+     }
+   };
+
+   const vname = (value) => {
+     if (value.length < 3 || value.length > 40) {
+       return (
+         <div>
+           The username must be between 3 and 40 characters.
+         </div>
+       );
+     }
+   };
+
+   const vpassword = (value) => {
+     if (value.length < 6 || value.length > 40) {
+       return (
+         <div>
+           The password must be between 6 and 40 characters.
+         </div>
+       );
+     }
+   };
+
+   const vpasswordConfirmation = (value) => {
+     if (value !== vpassword) {
+       return (
+         <div>
+           Should match the password above.
+         </div>
+       );
+     }
+   };
+
+   const vorganization = (value) => {
+     if (value.length < 6 || value.length > 50) {
+       return (
+         <div>
+           The password must be between 6 and 40 characters.
+         </div>
+       );
+     }
+   }
 
   const handleChange = (e) => {
     setState({
@@ -25,6 +91,10 @@ function Signup() {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    setSuccessful(false);
+
+    form.current.validateAll();
     const data = {
       name: state.name,
       email: state.email,
@@ -33,40 +103,75 @@ function Signup() {
       organization: state.organization,
     };
     console.log(data);
-     console.log(process.env.REACT_APP_KEY);
-     console.log('api:', process.env.REACT_APP_API);
-    axios
-      .post(process.env.REACT_APP_API + '/users/signup', state, {
-        headers: {
-          'X-BLK-CROSS-KEY': process.env.REACT_APP_KEY,
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-      })
-      .then(function (data) {
-        console.log(data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      }); 
+    if (checkBtn.current.context._errors.length === 0) {
+      dispatch(
+        register(data)  
+      )
+        .then(() => {
+          setSuccessful(true);
+          // window.location.href = '/success';
+        })
+        .catch(() => {
+          setSuccessful(false);
+        });
+    }
+
+    // const data = {
+    //   name: state.name,
+    //   email: state.email,
+    //   password: state.password,
+    //   passwordConfirmation: state.passwordConfirmation,
+    //   organization: state.organization,
+    // };
+    // console.log(data);
+    //  console.log(process.env.REACT_APP_KEY);
+    //  console.log('api:', process.env.REACT_APP_API);
+    // axios
+    //   .post(process.env.REACT_APP_API + '/users/signup', state, {
+    //     headers: {
+    //       'X-BLK-CROSS-KEY': process.env.REACT_APP_KEY,
+    //       Accept: 'application/json',
+    //       'Content-Type': 'application/json',
+    //     },
+    //   })
+    //   .then(function (data) {
+    //     console.log(data);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+
     // window.location.href = '/success';
     //connect with API etc...
   };
+  
   return (
     <>
-      <SignupForm
-        name={state.name}
-        handleChangeName={handleChange}
-        email={state.email}
-        handleChangeEmail={handleChange}
-        password={state.password}
-        handleChangePassword={handleChange}
-        PasswordConfirmation={state.passwordConfirmation}
-        handleChangePasswordConfirmation={handleChange}
-        organization={state.organization}
-        handleChangeOrganization={handleChange}
-        handleSubmit={handleSubmit}
-      />
+      {!successful && (
+        <SignupForm
+          refForm={form}
+          name={state.name}
+          nameValidation={[required, vname]}
+          handleChangeName={handleChange}
+          email={state.email}
+          emailValidation={[required, validEmail]}
+          handleChangeEmail={handleChange}
+          password={state.password}
+          passwordValidation={[required, vpassword]}
+          handleChangePassword={handleChange}
+          PasswordConfirmation={state.passwordConfirmation}
+          passwordVapasswordConfirmationValidationlidation={[
+            required,
+            vpasswordConfirmation,
+          ]}
+          handleChangePasswordConfirmation={handleChange}
+          organization={state.organization}
+          organizationValidation={[required, vorganization]}
+          handleChangeOrganization={handleChange}
+          handleSubmit={handleSubmit}
+          checkButton={checkBtn}
+        />
+      )}
     </>
   );
 }
